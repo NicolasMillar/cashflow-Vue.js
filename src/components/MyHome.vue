eb6ccc236ebdcae71db3b8365c29b0526eeb50db
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2023-04-16 16:03:01 -0400

    creacion de vista resumen+movimientos

--- src/components/MyHome.vue
+++ src/components/MyHome.vue
@@ -3,17 +3,27 @@
     <template #header>
       <Header />
     </template>
+    <template #resume>
+      <Resume />
+    </template>
+    <template #movements>
+      <Movements />
+    </template>
   </Layout>
 </template>
 
 <script>
 import Layout from "./MyLayout.vue";
 import Header from "./MyHeader.vue";
+import Resume from "./Resume/ResumeIndex.vue";
+import Movements from "./MyMovements.vue";
 
 export default {
   components: {
     Layout,
     Header,
+    Resume,
+    Movements,
   },
 };
 </script>
